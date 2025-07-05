51c69dccbd7ff1743fa922a9b5eaa532edf1cd6d
Lilly-020 <lillycardoso098@gamil.co>
2025-07-05 10:03:59 -0300

    Mudanças de localização de pastas e ajustes

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.abspath("templates"), static_folder=os.path.abspath("static"))
 
 @app.route("/")
 def index():
-    pass
+    return render_template('index.html')
 
 @app.route("/inicio")
 def inicio():
-    pass
+    return render_template('inicio.html')
 
 @app.route("/animation")
 def animation():
     pass
 
-@app.route("/laptop")
-def laptop():
-    pass
+@app.route("/play")
+def play():
+    return render_template('play.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
