619c139ea9d8419039d922ddce7496812483853e
Lilly-020 <lillycardoso098@gamil.co>
2025-07-15 19:05:27 -0300

    porta

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def animation():
     return render_template('note_animacao.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
